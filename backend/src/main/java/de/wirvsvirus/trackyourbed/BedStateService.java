e5510a9b45175dd7935a5df897d888f557f316f4
Anemone <anemone@kampkoetter.de>
2020-03-22 00:29:15 +0100

    implemented bed state logic

---
+++ backend/src/main/java/de/wirvsvirus/trackyourbed/BedStateService.java
@@ -0,0 +1,29 @@
+package de.wirvsvirus.trackyourbed;
+
+import de.wirvsvirus.trackyourbed.entity.BedState;
+import de.wirvsvirus.trackyourbed.dto.response.BedStateDto;
+import de.wirvsvirus.trackyourbed.dto.response.mapper.BedStateDtoMapper;
+import de.wirvsvirus.trackyourbed.persistence.BedStateRepository;
+import java.util.NoSuchElementException;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BedStateService {
+  private final BedStateRepository bedStateRepository;
+  private final BedStateDtoMapper bedStateDtoMapper;
+
+  @Inject
+  public BedStateService(final BedStateRepository bedStateRepository,
+                         final BedStateDtoMapper bedStateDtoMapper){
+    this.bedStateRepository = bedStateRepository;
+    this.bedStateDtoMapper = bedStateDtoMapper;
+  }
+
+  public BedStateDto getBedStateByName(final String name) {
+      final BedState fetched = bedStateRepository.getByName(name)
+          .orElseThrow(NoSuchElementException::new);
+      return bedStateDtoMapper.entityToDto(fetched);
+
+  }
+}
---
+++ backend/src/main/resources/db/migration/V2020.03.21.3_add_bed_states.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.bed_state(name)
+VALUES('occupied');
+
+INSERT INTO public.bed_state(name)
+VALUES('free');
+
+INSERT INTO public.bed_state(name)
+VALUES('maybe');
+
+INSERT INTO public.bed_state(name)
+VALUES('out_of_order');
+
+INSERT INTO public.bed_state(name)
+VALUES('unknown');
