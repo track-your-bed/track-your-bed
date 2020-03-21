b95367db968cd47bb6d96286c8030e505cc8ab09
Michael Schubert <michael.schubert@tngtech.com>
2020-03-21 20:56:00 +0100

    add get all functionality for bed and station type

--- backend/src/main/java/de/wirvsvirus/trackyourbed/BedTypeService.java
+++ backend/src/main/java/de/wirvsvirus/trackyourbed/BedTypeService.java
@@ -4,6 +4,8 @@ import de.wirvsvirus.trackyourbed.dto.response.BedTypeDto;
 import de.wirvsvirus.trackyourbed.dto.response.mapper.BedTypeDtoMapper;
 import de.wirvsvirus.trackyourbed.entity.BedType;
 import de.wirvsvirus.trackyourbed.persistence.BedTypeRepository;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.NoSuchElementException;
 import javax.inject.Inject;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,12 @@ public class BedTypeService {
     final BedType fetched = bedTypeRepository.getByName(name)
         .orElseThrow(NoSuchElementException::new);
     return bedTypeDtoMapper.entityToDto(fetched);
+  }
+
+  public Collection<BedTypeDto> getAllBedTypes() {
+    final Collection<BedTypeDto> allBedTypes = new ArrayList<>();
+    bedTypeRepository.findAll().forEach(entity -> allBedTypes.add(bedTypeDtoMapper.entityToDto(entity)));
+    return allBedTypes;
   }
 
 }
--- backend/src/main/java/de/wirvsvirus/trackyourbed/resource/BedTypeResource.java
+++ backend/src/main/java/de/wirvsvirus/trackyourbed/resource/BedTypeResource.java
@@ -2,6 +2,7 @@ package de.wirvsvirus.trackyourbed.resource;
 
 import de.wirvsvirus.trackyourbed.BedTypeService;
 import de.wirvsvirus.trackyourbed.dto.response.BedTypeDto;
+import java.util.Collection;
 import javax.inject.Inject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,11 @@ public class BedTypeResource {
   @Inject
   public BedTypeResource(final BedTypeService bedTypeService) {
     this.bedTypeService = bedTypeService;
+  }
+
+  @GetMapping
+  public ResponseEntity<Collection<BedTypeDto>> getAllBedTypes() {
+    return ResponseEntity.ok(bedTypeService.getAllBedTypes());
   }
 
   @GetMapping("{name}")
