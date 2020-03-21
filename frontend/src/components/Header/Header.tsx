6df11b9914723cef512be499e91e3fc93eda977b
Maximilian Schmidt <mschmidt@mxmediendesign.de>
2020-03-21 19:33:13 +0100

    Minor Changes to Header
    * Switch to Functional Component
    * Remove unnecessary import

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,27 +1,20 @@
 import * as React from "react";
-import {
-    BrowserRouter as Router,
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-interface IHeader{
-    title: string;
+interface Header {
+  title: string;
 }
 
-const Header: React.FunctionComponent<IHeader> = ({title}:IHeader) => {
-    return(
-        <header>
-            <div className="header-home">
-                <Link to="/">Home</Link>
-            </div>
-            <div className="header-sidetitle">
-                {title}
-            </div>
-            <div className = "header-profile">
-                <Link to="/profile">Profil</Link>
-            </div>
-        </header>
-    );
-};
+const Header: React.FunctionComponent<Header> = ({ title }: Header) => (
+  <header>
+    <div className="header-home">
+      <Link to="/">Home</Link>
+    </div>
+    <div className="header-sidetitle">{title}</div>
+    <div className="header-profile">
+      <Link to="/profile">Profil</Link>
+    </div>
+  </header>
+);
 
 export default Header;
