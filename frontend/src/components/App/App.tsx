8599d96bbdb17ad2ce2b77cab625cf8c7155f5e2
Maximilian Schmidt <mschmidt@mxmediendesign.de>
2020-03-21 13:08:42 +0100

    Further Setup Changes
    - remove unneded assets
    - add typescript basics to app.tsx

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,26 +1,22 @@
-import React from "react";
-import logo from "../../logo.svg";
+import * as React from "react";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FunctionComponent = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
