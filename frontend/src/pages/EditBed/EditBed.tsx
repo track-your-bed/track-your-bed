edc4d788209de78be57c96eb6cd871a3790211d8
Grimm Julia <jg120@hdm-stuttgart.de>
2020-03-22 09:13:55 +0100

    Form for Edits

--- frontend/src/pages/EditBed/EditBed.tsx
+++ frontend/src/pages/EditBed/EditBed.tsx
@@ -1,3 +1,50 @@
-//Forms
-// -Name
-// -Type (Dropdown)
+
+import * as React from "react";
+import {Dropdown} from 'primereact/dropdown';
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+const divStyle = {
+  margin: '50px'
+};
+
+const btnStyle = {
+  margin: '20px'
+};
+
+const fieldStyle = {
+  width: '250px'
+}
+
+
+const beds = [
+    {label: 'Intensivbett', value: 'Intensivbett'},
+    {label: 'Normalbett', value: 'Normalbett'}
+];
+
+const EditBed: React.FunctionComponent = () => {
+  return (
+    <div style={divStyle}>
+      <h1>Bett hinzufügen</h1>
+        <form>
+          <Dropdown style={fieldStyle} options={beds}  placeholder="Bettart"/>
+          <br/>
+          <br/>
+          <span className="p-float-label">
+            <InputText id="in" style={fieldStyle}/>
+            <label htmlFor="in">Bettname</label>
+          </span>
+          <div>
+            <Button label="Hinzufügen" />
+            <Button style={btnStyle} label="Abbrechen" />
+          </div>
+        </form>
+    </div>
+  );
+};
+
+export default EditBed;
--- frontend/src/pages/EditDepartment/EditDepartment.tsx
+++ frontend/src/pages/EditDepartment/EditDepartment.tsx
@@ -1,3 +1,52 @@
-//Fields:
-// -Name
-// -
+
+
+import * as React from "react";
+import {Dropdown} from 'primereact/dropdown';
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+const divStyle = {
+  margin: '50px'
+};
+
+const btnStyle = {
+  margin: '20px'
+};
+
+const fieldStyle = {
+  width: '250px'
+}
+
+
+const departments = [
+  {label: 'Kardiologie', value: 'Kardiologie'},
+  {label: 'Urologie', value: 'Urologie'},
+  {label: 'Radiologie', value: 'Radiologie'}
+];
+
+const EditDepartment: React.FunctionComponent = () => {
+  return (
+    <div style={divStyle}>
+      <h1>Fachabteilung hinzufügen</h1>
+        <form>
+          <Dropdown style={fieldStyle} options={departments}  placeholder="Wählen Sie eine Fachabteilung"/>
+          <br/>
+          <br/>
+          <span className="p-float-label">
+            <InputText id="in" style={fieldStyle}/>
+            <label htmlFor="in">Name der Abteilung</label>
+          </span>
+          <div>
+            <Button label="Hinzufügen" />
+            <Button style={btnStyle} label="Abbrechen" />
+          </div>
+        </form>
+    </div>
+  );
+};
+
+export default EditDepartment;
