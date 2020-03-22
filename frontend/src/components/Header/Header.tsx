b9ef50ff6d4e68d11f5499c790f4b86671af14e3
wermar <marcel.wernisch@outlook.de>
2020-03-22 11:19:12 +0100

    add new Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,20 +1,65 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {Menubar} from 'primereact/menubar';
+import { useHistory } from 'react-router-dom'
 
 interface Header {
-  title: string;
+    title: string;
 }
 
-const Header: React.FunctionComponent<Header> = ({ title }: Header) => (
-  <header>
-    <div className="header-home">
-      <Link to="/">Home</Link>
-    </div>
-    <div className="header-sidetitle">{title}</div>
-    <div className="header-profile">
-      <Link to="/profile">Profil</Link>
-    </div>
-  </header>
-);
+const Header: React.FunctionComponent<Header> = ({title}: Header) => {
 
+    const history = useHistory();
+
+    const navigateToPage = (path: string) => {
+        console.log('Navigate to path ' + path);
+        history.push(path);
+    };
+
+    const items = [
+        {
+            label: 'Mein Klinikum',
+            icon: 'pi pi-home',
+            items: [
+                {
+                    label: 'Stammdatenverwaltung',
+                    command: () => {
+                        navigateToPage('/list')
+                    }
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Stationsbettenmanagement',
+                    command: () => {
+                        navigateToPage('/wardBedManagement')
+                    }
+                },
+            ]
+        },
+        {
+            label: 'Mein Profil',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'Settings',
+                    icon: 'pi pi-fw pi-cog',
+                    command:()=>{ navigateToPage('/userSettings')}
+                },
+            ]
+        },
+        {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off',
+            command: () => {
+                navigateToPage('/logout')
+            }
+        }
+    ];
+    return (<header>
+            <Menubar model={items}/>
+        </header>
+    );
+};
 export default Header;
