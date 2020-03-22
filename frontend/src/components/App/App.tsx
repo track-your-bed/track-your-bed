8c44ec3dc019bf0263b8359fd3cb31712783833d
wermar <marcel.wernisch@outlook.de>
2020-03-22 14:00:12 +0100

    add user details dialog

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -6,7 +6,7 @@ import Header from "../Header/Header";
 
 // Pages
 import Dashboard from "../../pages/Dashboard/Dashboard";
-import Settings from "../../pages/Settings/Settings";
+import UserSettings from "../../pages/UserSettings/UserSettings";
 import Login from "../../pages/Login/Login";
 import EditBed from "../../pages/EditBed/EditBed";
 import EditWard from "../../pages/EditWard/EditWard";
@@ -30,9 +30,6 @@ const App: React.FunctionComponent = () => (
         </Route>
         <Route path="/list" exact>
           <ListView />
-        </Route>
-        <Route path="/settings" exact>
-          <Settings />
         </Route>
         <Route path="/hospital/:hospitalId/:departmentID/:wardId/:bedId/edit">
           <EditBed />
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import {Link} from "react-router-dom";
 import {Menubar} from 'primereact/menubar';
 import { useHistory } from 'react-router-dom'
+import UserSettings from "../../pages/UserSettings/UserSettings";
+import {Dialog} from "primereact/dialog";
+import Label from "../Label/Label";
 
 interface Header {
     title: string;
@@ -10,10 +13,14 @@ interface Header {
 const Header: React.FunctionComponent<Header> = ({title}: Header) => {
 
     const history = useHistory();
-
+    const [visible, setVisible] = React.useState(false);
     const navigateToPage = (path: string) => {
         console.log('Navigate to path ' + path);
         history.push(path);
+    };
+
+    function openUserDialog () {
+        setVisible(true)
     };
 
     const items = [
@@ -45,7 +52,7 @@ const Header: React.FunctionComponent<Header> = ({title}: Header) => {
                 {
                     label: 'Settings',
                     icon: 'pi pi-fw pi-cog',
-                    command:()=>{ navigateToPage('/userSettings')}
+                    command:()=>{openUserDialog()}
                 },
             ]
         },
@@ -59,6 +66,12 @@ const Header: React.FunctionComponent<Header> = ({title}: Header) => {
     ];
     return (<header>
             <Menubar model={items}/>
+
+            <Dialog header="Benutzer Einstellungen" visible={visible} style={{width: '50vw'}} modal={true}
+                    onHide={() => setVisible(false)} position="top">
+                <UserSettings />
+
+            </Dialog>
         </header>
     );
 };
