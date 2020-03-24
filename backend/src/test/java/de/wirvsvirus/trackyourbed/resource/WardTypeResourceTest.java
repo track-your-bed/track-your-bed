a1e479266df6a2ac3d0fdab8e97a6ae77a972b9a
MMoll90 <45754174+MMoll90@users.noreply.github.com>
2020-03-24 22:48:37 +0100

    Added proper testing for ResponseEntity and improved Layout in WardTypeResourceTest

--- backend/src/test/java/de/wirvsvirus/trackyourbed/resource/WardTypeResourceTest.java
+++ backend/src/test/java/de/wirvsvirus/trackyourbed/resource/WardTypeResourceTest.java
@@ -16,79 +16,71 @@ import javax.ws.rs.core.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-@DisplayName("Tests for WardTypeResource")
+@DisplayName("Tests for WardTypeResource.")
 class WardTypeResourceTest {
 
   @Nested
-  @DisplayName("Test calls to getAllWardTypes")
+  @DisplayName("Test calls to getAllWardTypes.")
   class GetAllWardTypesTest {
 
     @Test
-    @DisplayName("Should return a response entity containing " +
-        "all ward types with status set to ok when called")
+    @DisplayName("Should return a response entity containing all ward types with status set to ok " +
+        "when called.")
     void shouldReturnResponseEntityWithAllWardTypesInBodyAndStatusSetToOkWhenCalled () {
-
       //GIVEN
-      final WardTypeService wardTypeService = mock(WardTypeService.class);
-
       final WardTypeDto wardOne = new WardTypeDto();
       final WardTypeDto wardTwo = new WardTypeDto();
       final String wardNameOne = "one";
       final String wardNameTwo = "two";
-
       wardOne.setName(wardNameOne);
       wardOne.setName(wardNameTwo);
-
       final Collection<WardTypeDto> allWardTypes = List.of(
           wardOne,
           wardTwo
       );
+      final WardTypeService wardTypeService = mock(WardTypeService.class);
       when(wardTypeService.getAllWardTypes()).thenReturn(allWardTypes);
 
-      final ResponseEntity<Collection<WardTypeDto>> expected = ResponseEntity.ok(allWardTypes);
-
       //WHEN
-      final ResponseEntity<Collection<WardTypeDto>> actual = new WardTypeResource(wardTypeService).getAllWardTypes();
+      final ResponseEntity<Collection<WardTypeDto>> actual
+          = new WardTypeResource(wardTypeService).getAllWardTypes();
 
       //THEN
-
-      assertEquals(actual,expected);
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(allWardTypes, actual.getBody());
     }
+
   }
 
   @Nested
-  @DisplayName("Test calls to getWardTypeByName")
+  @DisplayName("Test calls to getWardTypeByName.")
   class GetWardTypeByNameTest {
 
-
-
     @Test
-    @DisplayName("Should return response entity with the corresponding WardTypeDto" +
-        "and Status set to OK When Called with name")
+    @DisplayName("Should return response entity with the corresponding WardTypeDto and Status set to" +
+        " Ok when called with name.")
     void shouldReturnResponseEntityWithCorrectWardTypeAndStatusOkWhenCalledWithName () {
       //GIVEN
       final String name = "name";
-      final WardTypeService wardTypeService = mock(WardTypeService.class);
       final WardTypeDto wardTypeDto = new WardTypeDto();
       wardTypeDto.setName(name);
+      final WardTypeService wardTypeService = mock(WardTypeService.class);
       when(wardTypeService.getWardTypeByName(anyString())).thenReturn(wardTypeDto);
 
-      final ResponseEntity<WardTypeDto> expected = ResponseEntity.ok(wardTypeDto);
-
       //WHEN
-      final ResponseEntity<WardTypeDto> actual = new WardTypeResource(wardTypeService).getWardTypeByName(name);
+      final ResponseEntity<WardTypeDto> actual =
+          new WardTypeResource(wardTypeService).getWardTypeByName(name);
 
       //THEN
-
-      assertEquals(actual,expected);
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(wardTypeDto, actual.getBody());
 
       verify(wardTypeService).getWardTypeByName(eq(name));
-
     }
 
   }
-
 
 }
