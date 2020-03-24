4c97864bdd88b0509d67f5abaf9c3a46bec227c1
MMoll90 <45754174+MMoll90@users.noreply.github.com>
2020-03-24 13:15:14 +0100

    First version of test for getAllBeds of BedResourceTest

--- backend/src/test/java/de/wirvsvirus/trackyourbed/resource/BedResourceTest.java
+++ backend/src/test/java/de/wirvsvirus/trackyourbed/resource/BedResourceTest.java
@@ -2,12 +2,18 @@ package de.wirvsvirus.trackyourbed.resource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 import de.wirvsvirus.trackyourbed.BedService;
+import de.wirvsvirus.trackyourbed.dto.request.CreateNewBed;
+import de.wirvsvirus.trackyourbed.dto.response.BedDto;
+import java.util.Collection;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 @DisplayName("Tests for BedResource")
 class BedResourceTest {
@@ -21,6 +27,9 @@ class BedResourceTest {
         "when called with createNewbed")
     void ShouldReturnResponseEntityWithCreatedLinkAndBedDtoWhenCalledWithCreateNewBed() {
       final BedService bedService = mock(BedService.class);
+      final CreateNewBed createNewBed = mock(CreateNewBed.class);
+      //TO BE COMPLETED!!!!!!!
+
 
     }
 
@@ -31,8 +40,32 @@ class BedResourceTest {
   @Nested
   @DisplayName("Test calls to getAllBeds")
   class GetAllBedsTest {
-    final BedService bedService = mock(BedService.class);
 
+    @Test
+    @DisplayName("Should return a ResponseEntity containing a collection of all beds" +
+        "with status set to ok")
+    void shouldReturnResponseEntityContainingCollectionOfAllBedsWithStatusOkWhenCalled() {
+      //GIVEN
+      final BedService bedService = mock(BedService.class);
+      final BedDto bedOne = mock(BedDto.class);
+      final BedDto bedTwo = mock(BedDto.class);
+      final Collection<BedDto> allBeds = List.of(
+          bedOne,
+          bedTwo
+      );
+      final ResponseEntity<Collection<BedDto>> expected = ResponseEntity.ok(allBeds);
+      when(bedService.getAllBeds()).thenReturn(allBeds);
+
+      //WHEN
+
+      final ResponseEntity<Collection<BedDto>> actual = new BedResource(bedService).getAllBeds();
+
+
+      //THEN
+
+      assertEquals(actual,expected);
+
+    }
   }
 
   @Nested
