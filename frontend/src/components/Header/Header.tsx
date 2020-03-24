cc49902d311620d7fd1f6ace79978f0f53b51070
Maximilian Schmidt <mschmidt@mxmediendesign.de>
2020-03-24 14:05:38 +0100

    Refactor Header
    * Implement Bootstrap to Header
    * Remove MX and PrimeReact

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,78 +1,37 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
-import {Menubar} from 'primereact/menubar';
-import { useHistory } from 'react-router-dom'
-import UserSettings from "../../pages/UserSettings/UserSettings";
-import {Dialog} from "primereact/dialog";
-import Label from "../Label/Label";
+import { Link } from "react-router-dom";
+
+// Components
+import { Navbar, Nav } from "react-bootstrap";
 
 interface Header {
-    title: string;
+  title?: string;
 }
 
-const Header: React.FunctionComponent<Header> = ({title}: Header) => {
+const Header: React.FunctionComponent<Header> = ({ title }: Header) => (
+  <header>
+    <Navbar bg="light">
+      <Navbar.Brand>
+        <Link to="/">TrackYourBed</Link>
+      </Navbar.Brand>
+      <Navbar.Collapse>
+        <Nav className="w-100">
+          <Nav.Link as={Link} to="/list">
+            Stammdaten
+          </Nav.Link>
+          <Nav.Link as={Link} to="/wardBedManagement">
+            Bettenmanagement
+          </Nav.Link>
+          <Nav.Link as={Link} to="/settings">
+            Mein Profil
+          </Nav.Link>
+          <Nav.Link as={Link} to="/logout" className="ml-auto">
+            Logout
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </header>
+);
 
-    const history = useHistory();
-    const [visible, setVisible] = React.useState(false);
-    const navigateToPage = (path: string) => {
-        console.log('Navigate to path ' + path);
-        history.push(path);
-    };
-
-    function openUserDialog () {
-        setVisible(true)
-    };
-
-    const items = [
-        {
-            label: 'Mein Klinikum',
-            icon: 'pi pi-home',
-            items: [
-                {
-                    label: 'Stammdatenverwaltung',
-                    command: () => {
-                        navigateToPage('/list')
-                    }
-                },
-                {
-                    separator: true
-                },
-                {
-                    label: 'Stationsbettenmanagement',
-                    command: () => {
-                        navigateToPage('/wardBedManagement')
-                    }
-                },
-            ]
-        },
-        {
-            label: 'Mein Profil',
-            icon: 'pi pi-fw pi-user',
-            items: [
-                {
-                    label: 'Settings',
-                    icon: 'pi pi-fw pi-cog',
-                    command:()=>{openUserDialog()}
-                },
-            ]
-        },
-        {
-            label: 'Quit',
-            icon: 'pi pi-fw pi-power-off',
-            command: () => {
-                navigateToPage('/logout')
-            }
-        }
-    ];
-    return (<header>
-            <Menubar model={items}/>
-
-            <Dialog header="Benutzer Einstellungen" visible={visible} style={{width: '50vw'}} modal={true}
-                    onHide={() => setVisible(false)} position="top">
-                <UserSettings />
-
-            </Dialog>
-        </header>
-    );
-};
 export default Header;
