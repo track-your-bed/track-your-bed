1fddac0255e95df2717759533f9280e192d12751
MMoll90 <45754174+MMoll90@users.noreply.github.com>
2020-03-25 08:39:31 +0100

    Added proper testing for ResponseEntity and improved Layout in HospitalResourceTest

--- backend/src/test/java/de/wirvsvirus/trackyourbed/resource/HospitalResourceTest.java
+++ backend/src/test/java/de/wirvsvirus/trackyourbed/resource/HospitalResourceTest.java
@@ -1,7 +1,11 @@
 package de.wirvsvirus.trackyourbed.resource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -10,156 +14,185 @@ import de.wirvsvirus.trackyourbed.dto.request.CreateNewHospital;
 import de.wirvsvirus.trackyourbed.dto.request.UpdateHospital;
 import de.wirvsvirus.trackyourbed.dto.response.HospitalCapacityDto;
 import de.wirvsvirus.trackyourbed.dto.response.HospitalDto;
-import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-@DisplayName("Tests for HospitalResource")
+@DisplayName("Tests for HospitalResource.")
 class HospitalResourceTest {
 
   @Nested
-  @DisplayName("Test calls to createHospital")
+  @DisplayName("Test calls to createHospital.")
   class CreateHospitalTest {
 
     @Test
-    @DisplayName("Should return a response entity containing the created link and hospitalDto" +
-        "when called with createNewHospital")
+    @DisplayName("Should return a response entity containing the created link and hospitalDto when " +
+        "called with createNewHospital.")
     void ShouldReturnResponseEntityWithCreatedLinkAndHospitalDtoWhenCalledWithCreateNewHospital() {
       //GIVEN
-      final HospitalService hospitalService = mock(HospitalService.class);
       final CreateNewHospital createNewHospital = mock(CreateNewHospital.class);
       final UUID id = UUID.randomUUID();
-      final String createdLink = String.format("/api/hospitals/%s",id);
       final HospitalDto hospitalDto = new HospitalDto();
       hospitalDto.setId(id);
-      when(hospitalService.createHospital(createNewHospital)).thenReturn(hospitalDto);
-      final ResponseEntity<HospitalDto> expected = ResponseEntity
-          .created(URI.create(createdLink))
-          .body(hospitalDto);
+      final HospitalService hospitalService = mock(HospitalService.class);
+      when(hospitalService.createHospital(any(CreateNewHospital.class))).thenReturn(hospitalDto);
+
+      final List<String> expectedLink = List.of(String.format("/api/hospitals/%s",id));
 
       //WHEN
-      final ResponseEntity<HospitalDto> actual = new HospitalResource(hospitalService).createHospital(createNewHospital);
+      final ResponseEntity<HospitalDto> actual =
+          new HospitalResource(hospitalService).createHospital(createNewHospital);
 
       //THEN
-      assertEquals(expected,actual);
+      assertEquals(HttpStatus.CREATED, actual.getStatusCode());
+      assertEquals(expectedLink, actual.getHeaders().get("Location"));
+      assertSame(hospitalDto, actual.getBody());
+
+      verify(hospitalService).createHospital(eq(createNewHospital));
     }
+
   }
 
   @Nested
-  @DisplayName("Test calls to getAllHospitals")
+  @DisplayName("Test calls to getAllHospitals.")
   class GetAllHospitalsTest{
+
     @Test
-    @DisplayName("Should return a ResponseEntity containing a collection of all hospitals" +
-        "with status set to ok when called")
+    @DisplayName("Should return a ResponseEntity containing a collection of all hospitals with " +
+        "status set to ok when called.")
     void shouldReturnResponseEntityContainingCollectionOfAllHospitalsWithStatusOkWhenCalled() {
       //GIVEN
-      final HospitalService hospitalService = mock(HospitalService.class);
       final HospitalDto hospitalOne = mock(HospitalDto.class);
       final HospitalDto hospitalTwo = mock(HospitalDto.class);
       final Collection<HospitalDto> allHospitals = List.of(
           hospitalOne,
           hospitalTwo
       );
-      final ResponseEntity<Collection<HospitalDto>> expected = ResponseEntity.ok(allHospitals);
+      final HospitalService hospitalService = mock(HospitalService.class);
       when(hospitalService.getAllHospitals()).thenReturn(allHospitals);
 
       //WHEN
-      final ResponseEntity<Collection<HospitalDto>> actual = new HospitalResource(hospitalService).getAllHospitals();
-      //THEN
-      assertEquals(actual, expected);
+      final ResponseEntity<Collection<HospitalDto>> actual =
+          new HospitalResource(hospitalService).getAllHospitals();
 
+      //THEN
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(allHospitals, actual.getBody());
     }
 
   }
 
   @Nested
-  @DisplayName("Test calls to getHospitalById")
+  @DisplayName("Test calls to getHospitalById.")
   class GetHospitalTest {
+
     @Test
-    @DisplayName("Should return a ResponseEntity containing the correct HospitalDto" +
-        "with Status set to OK when called with  UUID id")
+    @DisplayName("Should return a ResponseEntity containing the correct HospitalDto with Status set " +
+        "to OK when called with  UUID id.")
     void shouldReturnResponseEntityWithHospitalDtoAndStatusOkWhenCalledWithId() {
       //GIVEN
-      final HospitalService hospitalService = mock(HospitalService.class);
       final HospitalDto hospitalDto = new HospitalDto();
       final UUID id = UUID.randomUUID();
       hospitalDto.setId(id);
-      when(hospitalService.getHospitalById(id)).thenReturn(hospitalDto); //any
-      final ResponseEntity<HospitalDto> expected = ResponseEntity.ok(hospitalDto);
+      final HospitalService hospitalService = mock(HospitalService.class);
+      when(hospitalService.getHospitalById(any(UUID.class))).thenReturn(hospitalDto);
 
       //WHEN
-      final ResponseEntity<HospitalDto> actual = new HospitalResource(hospitalService).getHospital(id);
+      final ResponseEntity<HospitalDto> actual =
+          new HospitalResource(hospitalService).getHospital(id);
+
       //THEN
-      assertEquals(expected, actual);
-      //verify....
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(hospitalDto, actual.getBody());
+
+      verify(hospitalService).getHospitalById(eq(id));
     }
+
   }
 
   @Nested
-  @DisplayName("Test calls to patchHospital")
+  @DisplayName("Test calls to patchHospital.")
   class PatchHospitalTest {
+
     @Test
-    @DisplayName("Should return a ResponseEntity containing the updated hospitalDto" +
-        "with Status set to OK when called with a UUID and an UpdateHospital")
+    @DisplayName("Should return a ResponseEntity containing the updated hospitalDto with Status set " +
+        "to OK when called with a UUID and an UpdateHospital.")
     void shouldReturnResponseEntityContainingHospitalDtoWithStatusOkWhenCalledWithUuidAndUpdateHospital() {
       //GIVEN
-      final HospitalService hospitalService = mock(HospitalService.class);
       final HospitalDto hospitalDto = new HospitalDto();
       final UpdateHospital updateHospital = new UpdateHospital();
       final UUID id = UUID.randomUUID();
-      when(hospitalService.updateHospital(id, updateHospital)).thenReturn(hospitalDto);
-      final ResponseEntity<HospitalDto> expected = ResponseEntity.ok(hospitalDto);
+      final HospitalService hospitalService = mock(HospitalService.class);
+      when(hospitalService.updateHospital(any(UUID.class), any(UpdateHospital.class))).
+          thenReturn(hospitalDto);
+
       //WHEN
-      final ResponseEntity<HospitalDto> actual = new HospitalResource(hospitalService).patchHospital(id, updateHospital);
+      final ResponseEntity<HospitalDto> actual =
+          new HospitalResource(hospitalService).patchHospital(id, updateHospital);
+
       //THEN
-      assertEquals(expected, actual);
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(hospitalDto, actual.getBody());
+
+      verify(hospitalService).updateHospital(eq(id), eq(updateHospital));
     }
 
   }
 
   @Nested
-  @DisplayName("Test calls to deleteHospital")
+  @DisplayName("Test calls to deleteHospital.")
   class DeleteHospitalTest {
+
     @Test
-    @DisplayName("Should return a ResponseEntity with empty body" +
-        "and status set to OK when called with Id ")
+    @DisplayName("Should return a ResponseEntity with empty body and status set to OK when called " +
+        "with Id.")
     void shouldReturnEmptyResponseEntityWithStatusOkWhenCalledWithId (){
       //GIVEN
       final HospitalService hospitalService = mock(HospitalService.class);
       final UUID id = UUID.randomUUID();
-      final ResponseEntity<Void> expected = ResponseEntity.ok().build();
-      //WHEN
-      final ResponseEntity<Void> actual = new HospitalResource(hospitalService).deleteHospital(id);
-      //THEN
-      assertEquals(expected,actual);
 
+      final ResponseEntity<Void> expected = ResponseEntity.ok().build();
+
+      //WHEN
+      final ResponseEntity<Void> actual =
+          new HospitalResource(hospitalService).deleteHospital(id);
+
+      //THEN
+      assertEquals(expected, actual);
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+
+      verify(hospitalService).deleteHospitalById(eq(id));
     }
   }
 
   @Nested
-  @DisplayName("Test calls to getCapacity")
+  @DisplayName("Test calls to getCapacity.")
   class GetCapacityTest{
+
     @Test
-    @DisplayName("Should return a ResponseEntity containing a HospitalCapacityDto with Status OK" +
-        "when called with an Id")
+    @DisplayName("Should return a ResponseEntity containing a HospitalCapacityDto with Status OK " +
+        "when called with an Id.")
     void shouldReturnResponseEntityContainingHospitalCapacityDtoWithStatusOkWhenCalledWithId(){
       //GIVEN
-      final HospitalService hospitalService = mock(HospitalService.class);
       final HospitalCapacityDto hospitalCapacityDto = mock(HospitalCapacityDto.class);
       final UUID id = UUID.randomUUID();
-      when(hospitalService.calculateCapacity(id)).thenReturn(hospitalCapacityDto);
-      final ResponseEntity<HospitalCapacityDto> expected = ResponseEntity.ok(hospitalCapacityDto);
+      final HospitalService hospitalService = mock(HospitalService.class);
+      when(hospitalService.calculateCapacity(any(UUID.class))).thenReturn(hospitalCapacityDto);
 
       //WHEN
-      final ResponseEntity<HospitalCapacityDto> actual = new HospitalResource(hospitalService).getCapacity(id);
+      final ResponseEntity<HospitalCapacityDto> actual =
+          new HospitalResource(hospitalService).getCapacity(id);
 
       //THEN
-      assertEquals(expected,actual);
+      assertEquals(HttpStatus.OK, actual.getStatusCode());
+      assertSame(hospitalCapacityDto, actual.getBody());
+
+      verify(hospitalService).calculateCapacity(eq(id));
     }
 
   }
