dcdd67cb5d74698d412bad6925ca5d975f83737b
Maximilian Schmidt <mschmidt@mxmediendesign.de>
2020-03-26 00:25:46 +0100

    prepare user logout

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,42 +1,51 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Button } from "react-bootstrap";
 
-// Components
-import { Navbar, Nav } from "react-bootstrap";
+// Context
+import { UserContext } from "../../contexts/UserContext";
 
 interface Header {
   title?: string;
 }
 
-const Header: React.FunctionComponent<Header> = ({ title }: Header) => (
-  <header className="mb-4">
-    <Navbar bg="light" expand="md">
-      <Navbar.Brand>
-        <Link to="/">TrackYourBed</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+const Header: React.FunctionComponent<Header> = ({ title }: Header) => {
+  const auth = React.useContext(UserContext);
 
-      <Navbar.Collapse>
-        <Nav className="w-100">
-          <Nav.Link as={Link} to="/list">
-            Stammdaten
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/wardBedManagement/989b78c4-1ae9-42d6-9408-686437dfc457"
-          >
-            Bettenmanagement
-          </Nav.Link>
-          <Nav.Link as={Link} to="/settings">
-            Mein Profil
-          </Nav.Link>
-          <Nav.Link as={Link} to="/logout" className="ml-auto">
-            Logout
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </header>
-);
+  return (
+    <header className="mb-4">
+      <Navbar bg="light" expand="md">
+        <Navbar.Brand>
+          <Link to="/">TrackYourBed</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="w-100">
+            <Nav.Link as={Link} to="/list">
+              Stammdaten
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/wardBedManagement/989b78c4-1ae9-42d6-9408-686437dfc457"
+            >
+              Bettenmanagement
+            </Nav.Link>
+            <Nav.Link as={Link} to="/settings">
+              Mein Profil
+            </Nav.Link>
+            <Nav.Link
+              as={Button}
+              variant="light"
+              onClick={(auth as any).logout}
+              className="ml-auto"
+            >
+              Logout
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
