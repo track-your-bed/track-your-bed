484c7dcc583539cae82261233807f110ea411dfa
Michael Schubert <michael.schubert@tngtech.com>
2020-03-27 20:14:38 +0100

    fix errors in local deployment

--- backend/flyway/build_flyway_image.sh
+++ backend/flyway/build_flyway_image.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p migration
-cp ../src/main/resources/db/migration/*.sql migration/
+mkdir -p migration_tmp
+cp ../src/main/resources/db/migration/*.sql migration_tmp/
 docker build . -t trackyourbed-flyway
+rm -r migration_tmp
